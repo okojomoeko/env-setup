1590140a1b459190fdc6d44b09c33e08b72c0b6c
okojomoeko <chikoro.size@gmail.com>
2022-07-17 10:45:45 +0900

    いろいろ修正

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # env-setup
+
+Mac と Win の開発環境を作る。
+
+それぞれ`lima`と`WSL2`を利用する想定。
+
+## Mac
+
+[]ima の install 方法](https://github.com/lima-vm/lima#getting-started)に従ってbrewでinstallする
+
+この Repository を clone してきて
+
+`limactl start path/env-setup/mac/lima/default.yaml`で instance 起動
+
+`lima`で instance に log in したときにうまく zsh とか`.zshrc`とかなかったら、一度 instance を再起動してから手動で適当に直す。(今後改善したい)
+
+VS Code から入れるように`.ssh/config`に追加
+
+```shell
+touch ~/.ssh/config && limactl show-ssh -f config default >> ~/.ssh/config
+```
+
+## TODO
+
+- [ ] 一部brewで管理外のアプリについてbrew管理できるようにする
+    - [ ] brave-browser
+    - [ ] google-drive
+    - [ ] karabiner-elements
+    - [ ] google-japanese-ime
+
+- [ ] windowsのwsl2でもbrew管理ができるようにする
+- [ ] virtual machine環境でもbrewで管理できるようにする
---
+++ mac/setup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+if [ "$(uname)" == 'Darwin' ]; then
+  echo "Install XCode"
+  xcode-select --install
+
+  echo "Install Rosetta2"
+  sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
+  BREW_PATH="/opt/homebrew"
+
+  echo "Install Homebrew"
+  which $BREW_PATH/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
+elif [ "$(uname)" == 'Linux' ]; then
+  sudo apt-get update
+  sudo apt-get install build-essential procps curl file git -y
+  BREW_PATH="/home/linuxbrew/.linuxbrew"
+  echo "Install Homebrew"
+  which $BREW_PATH/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zprofile
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bash_profile
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  git config --global core.autoCRLF false
+  # WSL 用の調整
+  if [[ "$(uname -r)" == *microsoft* ]]; then
+    echo "WSL"
+  fi
+
+else
+  echo 'Windows'
+fi
+
+
+#------------------------------------------
+# Homebrew
+#------------------------------------------
+echo "Install Homebrew"
+which $BREW_PATH/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "brew update and upgrade"
+which $BREW_PATH/bin/brew >/dev/null 2>&1 && brew update && brew upgrade
+
+echo "brew install"
+which $BREW_PATH/bin/brew >/dev/null 2>&1 && brew bundle --file ./Brewfile
+
+echo "brew cleanup"
+which brew >/dev/null 2>&1 && brew cleanup
+
+echo "$BREW_PATH/bin/zsh" | sudo tee -a /etc/shells
+sudo chsh -s $BREW_PATH/bin/zsh
+sudo chsh -s $BREW_PATH/bin/zsh $USER
+
+#------------------------------------------
+# Develop Environment
+#------------------------------------------
+./setup_devenv.sh
+
+
+#------------------------------------------
+# dotfiles
+#------------------------------------------
+./setup_dotfiles.sh
+
+#------------------------------------------
+# post setup for each os
+#------------------------------------------
+if [ "$(uname)" == 'Darwin' ]; then
+  echo "Mac"
+elif [ "$(uname)" == 'Linux' ]; then
+
+  # WSL 用の調整
+  if [[ "$(uname -r)" == *microsoft* ]]; then
+    echo "WSL"
+    #------------------------------------------
+    # Install Node.js
+    #------------------------------------------
+    # make cache folder (if missing) and take ownership
+    sudo mkdir -p /usr/local/n
+    sudo chown -R $USER /usr/local/n
+    # make sure the required folders exist (safe to execute even if they already exist)
+    sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+    # take ownership of Node.js install destination folders
+    sudo chown -R $USER /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+    n lts
+    #------------------------------------------
+    # Git credential helper
+    #------------------------------------------
+    git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
+  fi
+
+else
+  echo 'Windows'
+fi
+
+exec $SHELL -l
