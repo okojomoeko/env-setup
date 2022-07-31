01b9f2054fa1d07d3b0a750e5e3d9afbe6db4cec
okojomoeko <chikoro.size@gmail.com>
2022-07-31 18:53:44 +0900

    add container update script

---
+++ docker-devContainer/update_container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+# BASE_IMAGE="registry"
+# REGISTRY="registry.hub.docker.com"
+IMAGE="okojo-devcontainer"
+CID=$(docker ps -a --filter "name=$IMAGE" --filter "status=running"| grep $IMAGE | awk '{print $1}')
+
+echo $CID
+
+if [ -n "$CID" ]; then
+  docker exec -u 0 $CID /bin/bash -c "sudo apt update; sudo apt upgrade -y; sudo -s eval $(ncu -u -g | tail -n 2)"
+
+  exit;
+fi
