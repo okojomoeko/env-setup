440d336f36be3a854bde11175ba3b7c75f8f4c55
okojomoeko <chikoro.size@gmail.com>
2023-06-13 21:47:14 +0900

    macのsetup.sh

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
-set -euxo pipefail
+echo "Install XCode"
+xcode-select --install
 
-# 最新のgitをいれるためのrepository登録
-sudo add-apt-repository -y ppa:git-core/ppa;
-sudo apt update;
-sudo apt upgrade -y;
-sudo apt install -y zsh \
-                    unzip;
+echo "Install Rosetta2"
+sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
 
-# 自分のdotfilesをcloneしてdefault shellをzshに
-sudo chsh -s $(which zsh)
-sudo chsh -s $(which zsh) $USER
-exec -l $(which zsh)
-rm -rf ~/.zshrc
-rm -rf ~/.zsh_aliases
-git clone https://github.com/okojomoeko/dotfiles.git ~/work/env/dotfiles
-ln -s ~/work/env/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/work/env/dotfiles/zsh/.zsh_aliases ~/.zsh_aliases
-source ~/.zshrc
+#------------------------------------------
+# Homebrew
+#------------------------------------------
+echo "Install Homebrew"
+which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# # node install
-sudo apt install -y nodejs npm
-sudo npm update -g npm
-sudo npm install -g -y n
-sudo n stable
-sudo apt purge -y nodejs
-sudo npm install -g -y npm-check-updates
+echo "brew update and upgrade"
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew update && brew upgrade
 
-# install rust
-sudo apt-get install -y gcc
-curl https://sh.rustup.rs -sSf | bash -s -- -y
+echo "brew install"
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./Brewfile
 
-# # install python
-sudo apt install -y python3-pip
-curl -sSL https://install.python-poetry.org | python3 -
+echo "brew cleanup"
+which brew >/dev/null 2>&1 && brew cleanup
 
-# install aws cli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf aws awscliv2.zip
+#------------------------------------------
+# Develop Environment
+#------------------------------------------
+./setup_devenv.sh
 
-# set git config
-git config --global user.email "chikoro.size@gmail.com"
-git config --global user.name "okojomoeko"
+exec $SHELL -l
---
+++ mac/setup_devenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#------------------------------------------
+# Program language
+#------------------------------------------
+echo "Install Rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
