3216ca5392839ac4eb2805e2e719f919aa4a2adf
okojomoeko <chikoro.size@gmail.com>
2024-05-01 22:42:49 +0900

    GPUの設定を追加 (#7)

--- common/setup_devenv.sh
+++ common/setup_devenv.sh
@@ -17,17 +17,8 @@ elif [ "$(uname)" == 'Linux' ]; then
   # WSL 用の調整
   if [[ "$(uname -r)" == *microsoft* ]]; then
     echo "WSL"
-    #------------------------------------------
-    # Install Node.js
-    #------------------------------------------
-    # make cache folder (if missing) and take ownership
-    sudo mkdir -p /usr/local/n
-    sudo chown -R $USER /usr/local/n
-    # make sure the required folders exist (safe to execute even if they already exist)
-    sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
-    # take ownership of Node.js install destination folders
-    sudo chown -R $USER /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
-    n lts
+    $REPO_DIR/linux/setup_devenv_wsl.sh
+
     #------------------------------------------
     # Git credential helper
     #------------------------------------------
